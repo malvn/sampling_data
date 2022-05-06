bf66dae7fc921c522712a3324669f2e13ca1ed22
malvn <malvinrhrj@gmail.com>
2022-05-06 10:14:07 +0700

    Initial commit

---
+++ sampling.sh
@@ -0,0 +1,8 @@
+pip install csvkit
+python -m pip install sample-stream
+wget https://github.com/labusiam/dataset/raw/main/weather_data.xlsx
+in2csv weather_data.xlsx --sheet "weather_2014" > weather_2014.csv
+in2csv weather_data.xlsx --sheet "weather_2015" > weather_2015.csv
+csvstack weather_2014.csv weather_2015.csv > weather.csv
+rm weather_data.xlsx
+cat weather.csv | sample -r 0.3 > sample_weather.csv
